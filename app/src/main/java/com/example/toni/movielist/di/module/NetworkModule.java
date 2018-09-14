f22364812a01f637f9c4c98cb684501ba17f7552
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-09-14 16:24:04 +0200

    Preparing project for RxJava integration

--- app/src/main/java/com/example/toni/movielist/di/module/NetworkModule.java
+++ app/src/main/java/com/example/toni/movielist/di/module/NetworkModule.java
@@ -12,6 +12,7 @@ import dagger.Provides;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
@@ -42,6 +43,7 @@ public class NetworkModule {
         return new Retrofit.Builder()
                 .baseUrl(Constants.MOVIES_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(okHttpClient)
                 .build();
     }
